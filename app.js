bf021fe15428f98cf43850cbdbc0d2ff941dbeef
Logan Allred <allredlt@familysearch.org>
2017-10-03 14:29:26 -0600

    Switch to pure reveal slides
    Now mounted at /slides

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(logger('dev'));
 // Configure serving of static assets.
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'slides')));
+app.use('/slides', express.static(path.join(__dirname, 'slides')));
 app.use('/proxy', proxy({target: 'https://integration.familysearch.org', changeOrigin: true, pathRewrite: {'^/proxy' : ''}}));
 
 // Make the session available in templates and default to an empty object. If we
